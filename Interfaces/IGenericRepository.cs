1139cfdaaebefbf365f48a0199b54630b4c291c3
siddhraj-sinh <skrathod282001@gmail.com>
2023-06-22 18:10:14 +0530

    added interfaces

---
+++ Interfaces/IGenericRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogPost.Interfaces
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        T GetById(int id);
+        IEnumerable<T> GetAll();
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+
+
+    }
+}
---
+++ Repositories/GenericRepository.cs
@@ -0,0 +1,44 @@
+﻿using BlogPost.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogPost.Repositories
+{
+    internal class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+
+        protected readonly BloggingContext _context;
+
+        public GenericRepository(BloggingContext context)
+        {
+            _context = context;
+        }
+        public void Add(T entity)
+        {
+            _context.Set<T>().Add(entity);
+        }
+
+        public void Delete(T entity)
+        {
+            _context.Set<T>().Remove(entity);
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return _context.Set<T>().ToList();
+        }
+
+        public T GetById(int id)
+        {
+            return _context.Set<T>().Find(id);
+        }
+
+        public void Update(T entity)
+        {
+           _context.Set<T>().Update(entity);
+        }
+    }
+}
