cd299f063fb5c0d83cb46876f1dd94bea316e7c7
siddhraj-sinh <skrathod282001@gmail.com>
2023-06-22 12:01:36 +0530

    Add project files.

---
+++ Model.cs
@@ -0,0 +1,46 @@
+﻿
+
+using Microsoft.EntityFrameworkCore;
+
+public class BloggingContext : DbContext
+{
+    public DbSet<Blog> Blogs { get; set; }
+    public DbSet<Comment> Comments { get; set; }
+
+    public string DbPath { get; }
+    public BloggingContext()
+    {
+        var folder = Environment.SpecialFolder.LocalApplicationData;
+        var path = Environment.GetFolderPath(folder);
+        DbPath = System.IO.Path.Join(path, "blog.db");
+    }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseLazyLoadingProxies().UseSqlite($"Data Source={DbPath}");
+    }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Blog>().ToTable("MyBlogs");
+
+        //modelBuilder.Entity<Blog>().HasMany(b => b.Comments).WithOne(c => c.Blog).HasForeignKey(c => c.BlogId);
+    }
+}
+
+public class Blog
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Content { get; set; }
+    public virtual List<Comment> Comments { get; set; }
+}
+
+public class Comment
+{
+    public int Id { get; set; }
+    public string Content { get; set; }
+    public int BlogId { get; set; }
+    public virtual Blog Blog { get; set; }
+
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿
+
+using var db = new BloggingContext();
+
+db.Database.EnsureCreated();
+
+//create a new blog post
+
+var blog = new Blog { Title = "My first blog post" ,Content="This is my first blog post content.."};
+
+db.Blogs.Add(blog);
+db.SaveChanges();
+
+//create a comment for the blog post
+
+var comment1 = new Comment { Content = "Great Post !",BlogId=blog.Id };
+
+var comment2 = new Comment { Content = "I enjoyed reading this !",BlogId=blog.Id };
+db.Comments.AddRange(comment1, comment2);
+db.SaveChanges();
+
+var b = db.Blogs.FirstOrDefault(); // Retrieve a blog post
+
+if (b != null)
+{
+    Console.WriteLine($"Title: {b.Title}");
+    Console.WriteLine($"Content: {b.Content}");
+    Console.WriteLine("Comments:");
+
+    // Access the comments collection to trigger lazy loading
+    foreach (var comment in b.Comments)
+    {
+        Console.WriteLine($"- {comment.Content}");
+    }
+}
+//In this code, the blog post is retrieved using FirstOrDefault, but the comments are not loaded immediately. 
+// When you access the Comments collection within the foreach loop, lazy loading is triggered,
+// and the associated comments are loaded from the database at that point.
+
+
